ddea937535dcc0916f83883b6e8bbc9874c87cac
Jonathan Cobb <jonathan@kyuss.org>
2020-02-26 19:18:13 -0500

    add unique artifact id

--- src/main/java/allrightsy/Artifact.java
+++ src/main/java/allrightsy/Artifact.java
@@ -7,12 +7,17 @@ import lombok.ToString;
 import lombok.experimental.Accessors;
 import org.cobbzilla.util.collection.ArrayUtil;
 
+import static org.cobbzilla.util.daemon.ZillaRuntime.hashOf;
+
 @NoArgsConstructor @Accessors(chain=true) @ToString(of={"name", "type"})
 public class Artifact {
 
     @Getter @Setter private String name;
     @Getter @Setter private ArtifactType type;
     @Getter @Setter private License[] licenses;
+
+    @Getter(lazy=true) private final String id = hashOf(getName(), getType(), getLicenses());
+    public void setId () {} // noop
 
     public Artifact addLicense(License license) {
         licenses = ArrayUtil.append(licenses, license);
